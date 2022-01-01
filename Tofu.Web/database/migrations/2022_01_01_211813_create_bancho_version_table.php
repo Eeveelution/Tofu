0bb80b09cecb9a5ee0e826c1486ef1840d096878
Eevee <developerbrzoza@gmail.com>
2022-01-01 22:25:59 +0100

    Added Table for osu! versions

---
+++ Tofu.Web/database/migrations/2022_01_01_211813_create_bancho_version_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBanchoVersionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('osu_versions', function (Blueprint $table) {
+            $table->id();
+            $table->string("version", 16)->nullable(false)->index();
+            $table->string("version_handler", 16)->nullable(false)->index();
+            $table->boolean("allow_bancho")->default(true)->nullable(false);
+            $table->boolean("allow_scores")->default(true)->nullable(false);
+            $table->string("hash")->nullable(false)->unique()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bancho_version');
+    }
+}
